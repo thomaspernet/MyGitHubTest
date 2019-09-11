d87d56e24f4cfe35b663ff18241af9bea46cf6a3
thomaspernet <t.pernetcoudrier@gmail.com>
2019-09-11 14:58:50 +0200

    Update myFirstPython.py

--- myFirstPython.py
+++ myFirstPython.py
@@ -6,7 +6,7 @@ np.random.rand(3,3)
 
 ##### Create 2 random arrays
 x = 4 * np.random.rand(100)
-mu, sigma = 3, 3
+mu, sigma = 0, 1
 y =  2 + 3 * x +  np.random.normal(mu, sigma, 100)
 
 import matplotlib.pyplot as plt
@@ -16,3 +16,9 @@ plt.title(r"$ y = 2 + 3x + e $")
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
+
+from scipy import stats
+### Compute the linear relationship
+slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+print("The slope is {0:.2f} and the intercept is {1:.2f}".format(slope,
+ intercept))
